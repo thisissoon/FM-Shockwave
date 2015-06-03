cb685b1dc33bd52541ce4a5d28e46b711ba01b39
Chris Reeves <chris@thisissoon.com>
2015-06-03 18:55:56 +0100

    Command line flags

--- cmd/shockwave/shockwave.go
+++ cmd/shockwave/shockwave.go
@@ -7,8 +7,18 @@ import (
 	"github.com/thisissoon/FM-Shockwave"
 )
 
+// Flag Variable Holders
+var (
+	redis_address string
+	redis_channel string
+	max_volume    int
+	min_volume    int
+)
+
+// Long Command Description
 var shockWaveLongDesc = `Shockwave Manages the Volume Levels on the System`
 
+// Cobra Base Command
 var ShockWaveCmd = &cobra.Command{
 	Use:   "shockwave",
 	Short: "Volume Managment Service",
@@ -18,6 +28,15 @@ var ShockWaveCmd = &cobra.Command{
 	},
 }
 
+// Set Command Line Flags
+func init() {
+	ShockWaveCmd.Flags().StringVarP(&redis_address, "redis", "r", "127.0.0.1:6379", "Redis Server Address")
+	ShockWaveCmd.Flags().StringVarP(&redis_channel, "channel", "c", "", "Redis Channel Name")
+	ShockWaveCmd.Flags().IntVarP(&max_volume, "max_volume", "", 100, "Max Volume Level")
+	ShockWaveCmd.Flags().IntVarP(&min_volume, "min_volume", "", 0, "Min Volume Level")
+}
+
+// Application Entry Point
 func main() {
 	ShockWaveCmd.Execute()
 }
