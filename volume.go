d9d440ac922eb438e7055f5ba95f50d8428dfcbe
Chris Reeves <chris@thisissoon.com>
2015-06-04 09:24:12 +0100

    VolumeManage type w/ Constructor

--- volume.go
+++ volume.go
@@ -2,7 +2,27 @@
 
 package shockwave
 
-import "fmt"
+import (
+	"fmt"
+
+	"gopkg.in/redis.v3"
+)
+
+type VolumeManager struct {
+	ReidsClient  *redis.Client
+	RedisChannel *string
+	MaxVolume    *int
+	MinVolume    *int
+}
+
+func NewVolumeManager(r *redis.Client, c *string, max *int, min *int) *VolumeManager {
+	return &VolumeManager{
+		ReidsClient:  r,
+		RedisChannel: c,
+		MaxVolume:    max,
+		MinVolume:    min,
+	}
+}
 
 func Foo() {
 	fmt.Println("Foo")
