2aa5ac6863523c089c70a097ce0fdd138893d493
Chris Reeves <chris@thisissoon.com>
2015-09-03 14:49:16 +0100

    Cleanup

--- cmd/shockwave/shockwave.go
+++ cmd/shockwave/shockwave.go
@@ -7,10 +7,7 @@ import (
 	"os"
 	"os/signal"
 
-	"gopkg.in/redis.v3"
-
 	"github.com/spf13/cobra"
-	"github.com/thisissoon/FM-Shockwave"
 	"github.com/thisissoon/FM-Shockwave/event"
 	"github.com/thisissoon/FM-Shockwave/socket"
 	"github.com/thisissoon/FM-Shockwave/volume"
@@ -18,8 +15,6 @@ import (
 
 // Flag Variable Holders
 var (
-	redis_address  string
-	redis_channel  string
 	perceptor_addr string
 	secret         string
 	max_volume     int
@@ -38,27 +33,9 @@ var ShockWaveCmd = &cobra.Command{
 	Long:  shockWaveLongDesc,
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Println("Starting Shockwave")
-
-		// Create a redis client
-		redis_client := redis.NewClient(&redis.Options{
-			Network: "tcp",
-			Addr:    redis_address,
-		})
-
-		// Create Volume Manager
-		v := shockwave.NewVolumeManager(
-			redis_client,
-			&redis_channel,
-			&max_volume,
-			&min_volume,
-			&mixer,
-			&device)
-		go v.Subscribe() // Subscribe to the redis Pub/Sub channel and consume the messages
-
 		// Channel to listen for OS Signals
 		signals := make(chan os.Signal, 1)
 		signal.Notify(signals, os.Interrupt, os.Kill)
-
 		// Run for ever unless we get a signal
 		for sig := range signals {
 			log.Println(sig)
@@ -71,9 +48,6 @@ var ShockWaveCmd = &cobra.Command{
 func init() {
 	ShockWaveCmd.Flags().StringVarP(&perceptor_addr, "perceptor", "p", "perceptor.thisissoon.fm", "Perceptor Server Address")
 	ShockWaveCmd.Flags().StringVarP(&secret, "secret", "s", "CHANGE_ME", "Client Secret Ket")
-
-	ShockWaveCmd.Flags().StringVarP(&redis_address, "redis", "r", "127.0.0.1:6379", "Redis Server Address")
-	ShockWaveCmd.Flags().StringVarP(&redis_channel, "channel", "c", "", "Redis Channel Name")
 	ShockWaveCmd.Flags().IntVarP(&max_volume, "max_volume", "", 100, "Max Volume Level")
 	ShockWaveCmd.Flags().IntVarP(&min_volume, "min_volume", "", 0, "Min Volume Level")
 	ShockWaveCmd.Flags().StringVarP(&device, "device", "d", "default", "Audio Device Name")
